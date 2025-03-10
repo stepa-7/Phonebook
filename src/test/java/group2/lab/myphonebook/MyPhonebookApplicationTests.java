32e74298be68b5b502b78b35707a44707ce9b899
Stepan <sachumovitskii@edu.hse.ru>
2025-03-11 00:00:02 +0300

    Implemented REST API with CRUD operations for users, integrated with PostgreSQL database. Added unit tests for API endpoints.

--- src/test/java/group2/lab/myphonebook/MyPhonebookApplicationTests.java
+++ src/test/java/group2/lab/myphonebook/MyPhonebookApplicationTests.java
@@ -1,13 +1,67 @@
 package group2.lab.myphonebook;
 
+import group2.lab.myphonebook.contact.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class MyPhonebookApplicationTests {
+    @Autowired
+    private ContactService contactService; // Внедряем сервис
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void main() {
+    }
+
+    @Test
+    void addContact() {
+        Contact contact = new Contact(null, "John Doe", "1234567890", "Test Note");
+        Contact savedContact = contactService.addContact(contact);
+        assertNotNull(savedContact.getId());
+        assertEquals("John Doe", savedContact.getFullName());
+        assertEquals("1234567890", savedContact.getPhoneNumber());
+        assertEquals("Test Note", savedContact.getNote());
+    }
+
+    @Test
+    void updateContact() {
+        Contact contact = new Contact(null, "John Doe", "1234567890", "Test Note");
+        Contact savedContact = contactService.addContact(contact);
+        savedContact.setFullName("Jane Doe");
+        savedContact.setPhoneNumber("9876543210");
+        savedContact.setNote("Updated Note");
+        Contact updatedContact = contactService.addContact(savedContact);
+        assertEquals("Jane Doe", updatedContact.getFullName());
+        assertEquals("9876543210", updatedContact.getPhoneNumber());
+        assertEquals("Updated Note", updatedContact.getNote());
+    }
+
+    @Test
+    void deleteContact() {
+        int count = contactService.getAllContacts().size();
+        Contact contact = new Contact(null, "John Doe", "1234567890", "Test Note");
+        Contact savedContact = contactService.addContact(contact);
+        contactService.deleteContact(savedContact.getId());
+        assertEquals(count, contactService.getAllContacts().size());
+    }
+
+    @Test
+    void getContact() {
+        Contact contact = new Contact(null, "John Doe", "1234567890", "Test Note");
+        Contact savedContact = contactService.addContact(contact);
+        Optional<Contact> retrievedContact = contactService.getContact(savedContact.getId());
+        assertEquals("John Doe", retrievedContact.get().getFullName());
+        assertEquals("1234567890", retrievedContact.get().getPhoneNumber());
+        assertEquals("Test Note", retrievedContact.get().getNote());
+    }
 }

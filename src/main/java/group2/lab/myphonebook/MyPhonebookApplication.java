6bf086e88a5cbad1f972b369285960f43d6fcc6f
Stepan <sachumovitskii@edu.hse.ru>
2025-03-10 22:25:07 +0300

    Initial commit

---
+++ src/main/java/group2/lab/myphonebook/MyPhonebookApplication.java
@@ -0,0 +1,13 @@
+package group2.lab.myphonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhonebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPhonebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group2/lab/myphonebook/MyPhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package group2.lab.myphonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
